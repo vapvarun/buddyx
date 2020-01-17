b9d65a209a82ac2c14be5520a0ecbba8f4a0e1bb
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2020-01-17 19:34:26 +0530

    (#8) Header dropdown toggle issue fixed

--- inc/Custom_Js/Component.php
+++ inc/Custom_Js/Component.php
@@ -49,6 +49,17 @@ class Component implements Component_Interface {
 			return;
 		}
 
+		// Enqueue the moby script.
+		wp_enqueue_script(
+			'buddyx-doubletap',
+			get_theme_file_uri( '/assets/js/doubletap.min.js' ),
+			[],
+			buddyx()->get_asset_version( get_theme_file_path( '/assets/js/doubletap.min.js' ) ),
+			false
+		);
+		wp_script_add_data( 'buddyx-doubletap', 'async', true );
+		wp_script_add_data( 'buddyx-doubletap', 'precache', true );
+
 		// Enqueue the isotope script.
 		wp_enqueue_script(
 			'buddyx-isotope-pkgd',
