4dc51fa08230bb234e4f731a8bc79c7fedcc3092
Varun Kumar Dubey <vapvarun@gmail.com>
2020-01-23 21:48:44 +0530

    buddyx

--- external/class-tgm-plugin-activation.php
+++ external/class-tgm-plugin-activation.php
@@ -8,7 +8,7 @@
  * or theme author for support.
  *
  * @package   TGM-Plugin-Activation
- * @version   2.6.1 for parent theme Wp Rig for publication on WordPress.org
+ * @version   2.6.1 for parent theme BuddyX for publication on WordPress.org
  * @link      http://tgmpluginactivation.com/
  * @author    Thomas Griffin, Gary Jones, Juliette Reinders Folmer
  * @copyright Copyright (c) 2011, Thomas Griffin
