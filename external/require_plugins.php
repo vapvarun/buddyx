0f3f1965707837fcea3469da5485dbf50f4d3a4b
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2020-01-27 11:31:34 +0530

    Update theme files

--- external/require_plugins.php
+++ external/require_plugins.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file represents an example of the code that themes would use to register
  * the required plugins.
@@ -16,7 +17,6 @@
  * @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
  * @link       https://github.com/TGMPA/TGM-Plugin-Activation
  */
-
 /**
  * Include the TGM_Plugin_Activation class.
  *
@@ -53,32 +53,51 @@ add_action( 'tgmpa_register', 'buddyx_register_required_plugins' );
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
 function buddyx_register_required_plugins() {
+	$remote_url = 'https://demos.wbcomdesigns.com/exporter/plugins';
+
 	/*
 	 * Array of plugin arrays. Required keys are name and slug.
 	 * If the source is NOT from the .org repo, then source is also required.
 	 */
 	$plugins = array(
+		// Demo Impoter
+		array(
+			'name'		 => 'BuddyX Demo Importer',
+			'slug'		 => 'buddyx-demo-importer',
+			'source'	 => $remote_url . '/buddyx-demo-importer/1.0.0/buddyx-demo-importer.zip',
+			'required'	 => false,
+			'version'	 => '1.0.0',
+		),
 		// Kirki Toolkit
 		array(
 			'name'		 => esc_html__( 'Kirki Toolkit', 'buddyx' ),
 			'slug'		 => 'kirki',
 			'required'	 => true,
 		),
-
+		// Ultimate Addons for Gutenberg
+		array(
+			'name'		 => 'Ultimate Addons for Gutenberg',
+			'slug'		 => 'ultimate-addons-for-gutenberg',
+			'required'	 => false,
+		),
 		// BuddyPress Plugin
 		array(
 			'name'		 => 'BuddyPress',
 			'slug'		 => 'buddypress',
 			'required'	 => false,
 		),
-
 		// rtMedia Plugin
 		array(
 			'name'		 => 'rtMedia for WordPress, BuddyPress and bbPress',
 			'slug'		 => 'buddypress-media',
 			'required'	 => false,
 		),
-
+		// WooCommerce
+		array(
+			'name'		 => 'WooCommerce',
+			'slug'		 => 'woocommerce',
+			'required'	 => false,
+		),
 	);
 
 	/*
