2f8487b7da1e3a21420604b75d2ad3a8899685e4
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2020-02-19 17:03:44 +0530

    (#23) Update entry-full-width.php file code

--- template-parts/content/entry-full-width.php
+++ template-parts/content/entry-full-width.php
@@ -10,5 +10,27 @@ namespace BuddyX\Buddyx;
 ?>
 
 <div class="entry-content">
-	<?php the_content(); ?>
+	<?php
+	the_content(
+		sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'buddyx' ),
+				[
+					'span' => [
+						'class' => [],
+					],
+				]
+			),
+			get_the_title()
+		)
+	);
+
+	wp_link_pages(
+		[
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'buddyx' ),
+			'after'  => '</div>',
+		]
+	);
+	?>
 </div><!-- .entry-content -->
