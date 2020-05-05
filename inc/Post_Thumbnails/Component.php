12430f416f1845a4c8cbe9ce76b7c49448f1de1a
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2020-05-05 13:37:10 +0530

    Update welcome page code and theme fixes

--- inc/Post_Thumbnails/Component.php
+++ inc/Post_Thumbnails/Component.php
@@ -47,6 +47,6 @@ class Component implements Component_Interface {
 	 * Adds custom image sizes.
 	 */
 	public function action_add_image_sizes() {
-		add_image_size( 'buddyx-featured', 720, 480, true );
+		add_image_size( 'buddyx-featured', 720, 480 );
 	}
 }
