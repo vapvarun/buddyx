092b1b3325dfa505b32e4af05364cec505aab827
Varun Dubey <vapvarun@gmail.com>
2020-08-16 15:42:05 +0530

    Update info.php

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -12,12 +12,12 @@ $copyright = get_theme_mod( 'site_copyright_text' );
 
 	<div class="site-info">
 		<div class="container">
-			
+
 
 			<?php if ( $copyright ) {
 				echo ( $copyright );
 			} else {
-				 echo 'Copyright &copy; 2020. All rights reserved by, <a href="#">Brndle</a>';
+				 echo 'Copyright &copy; 2020. All rights reserved by, <a href="#">BuddyX Theme</a>';
 			} ?>
 		</div>
 
