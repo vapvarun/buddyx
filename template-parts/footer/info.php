c8994180a1e92b7aa1e0ed5c4ef312edb9e5ff86
Varun Dubey <vapvarun@gmail.com>
2020-08-25 00:27:29 +0530

    Update info.php

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -12,12 +12,12 @@ $copyright = get_theme_mod( 'site_copyright_text' );
 
 	<div class="site-info">
 		<div class="container">
-			
+
 
 			<?php if ( $copyright ) {
 				echo ( $copyright );
 			} else { ?>
-				<?php esc_html_e( 'Copyright &copy; 2020. All rights reserved by, ', 'wp-rig' ); ?><a href="<?php echo esc_url( '#' ); ?>"><?php esc_html_e( 'BuddyX Theme', 'buddyx' ); ?></a><?php
+				<?php esc_html_e( 'Copyright &copy; 2020. All rights reserved by, ', 'buddyx' ); ?><a href="<?php echo esc_url( '#' ); ?>"><?php esc_html_e( 'BuddyX Theme', 'buddyx' ); ?></a><?php
 			} ?>
 		</div>
 
