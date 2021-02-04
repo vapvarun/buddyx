e151e20272d22511249888b00524c88cbf1a9f20
Nitin Patil <nitin.wbcomdesigns@gmail.com>
2021-02-04 13:19:09 +0530

    PHPCS fixes (buddypress\members)

--- buddypress/members/single/home.php
+++ buddypress/members/single/home.php
@@ -7,31 +7,31 @@
  */
 ?>
 
-    <?php bp_nouveau_member_hook( 'before', 'home_content' ); ?>
+	<?php bp_nouveau_member_hook( 'before', 'home_content' ); ?>
 
 	<div id="item-header" role="complementary" data-bp-item-id="<?php echo esc_attr( bp_displayed_user_id() ); ?>" data-bp-item-component="members" class="users-header single-headers">
 
 		<?php bp_nouveau_member_header_template_part(); ?>
 
-    </div><!-- #item-header -->
-    
-    <div class="site-wrapper member-home">
-        <div class="bp-wrap">
-            <?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
+	</div><!-- #item-header -->
+	
+	<div class="site-wrapper member-home">
+		<div class="bp-wrap">
+			<?php if ( ! bp_nouveau_is_object_nav_in_sidebar() ) : ?>
 
-                <?php bp_get_template_part( 'members/single/parts/item-nav' ); ?>
+				<?php bp_get_template_part( 'members/single/parts/item-nav' ); ?>
 
-            <?php endif; ?>
+			<?php endif; ?>
 
-            <div id="item-body" class="item-body">
+			<div id="item-body" class="item-body">
 
-                <?php bp_nouveau_member_template_part(); ?>
+				<?php bp_nouveau_member_template_part(); ?>
 
-            </div><!-- #item-body -->
-        </div><!-- // .bp-wrap -->
-        <?php if ( get_theme_mod( 'buddypress_sidebar_option' ) == 'right' || get_theme_mod( 'buddypress_sidebar_option' ) == 'both' ) : ?>
-            <?php get_sidebar('buddypress'); ?> 
-        <?php endif; ?>
-    </div><!-- .site-wrapper -->
+			</div><!-- #item-body -->
+		</div><!-- // .bp-wrap -->
+		<?php if ( get_theme_mod( 'buddypress_sidebar_option' ) == 'right' || get_theme_mod( 'buddypress_sidebar_option' ) == 'both' ) : ?>
+			<?php get_sidebar( 'buddypress' ); ?> 
+		<?php endif; ?>
+	</div><!-- .site-wrapper -->
 
 	<?php bp_nouveau_member_hook( 'after', 'home_content' ); ?>
