c3d0d33d66cc354cd7496196c56e83b3489da7db
Nitin Patil <pallavi@wbcomdesigns.com>
2021-09-03 13:34:49 +0530

    (#314) Managed events manager events, single event page UI

--- template-parts/content/entry_header.php
+++ template-parts/content/entry_header.php
@@ -17,7 +17,8 @@ namespace BuddyX\Buddyx;
 	if ( ! is_singular() ) {
 		get_template_part( 'template-parts/content/entry_title', get_post_type() );
 	}
-
+	if ( is_singular( 'post' ) ) {
 		get_template_part( 'template-parts/content/entry_meta', get_post_type() );
+	}
 	?>
 </header><!-- .entry-header -->
