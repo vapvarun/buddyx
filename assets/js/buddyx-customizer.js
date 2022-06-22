3afcfac328fa864659fa2646d4ad911aa28a52de
dishantpatel <dishant.patel1007@gmail.com>
2022-06-22 14:38:28 +0530

    Added#431 - update js file to flush local font files

--- assets/js/buddyx-customizer.js
+++ assets/js/buddyx-customizer.js
@@ -1,10 +1,13 @@
-$(document).ready(function($) {
-    $(document).on("click", '.button.buddyx-flush-font-files', function(e) {
-        e.preventDefault();
-        var data = {
-            'action': 'buddyx_regenerate_fonts_folder',
-        };
+( function( $ ) {
+	"use strict";
+	$(document).ready(function($) {
+		$(document).on("click", '.button.buddyx-flush-font-files', function(e) {
+			e.preventDefault();
+			var data = {
+				'action': 'buddyx_regenerate_fonts_folder',
+			};
 
-        $.post(ajaxurl, data, function(response) {});
-    });
-});
+			$.post(ajaxurl, data, function(response) {});
+		});
+	});
+}( jQuery ) );
