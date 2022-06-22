78854e546ba91baea5a3f626d5c9c7d9ddea21e8
Nitin Patil <pallavi@wbcomdesigns.com>
2022-06-22 14:33:48 +0530

    (#431) Added buddyx customizer js file

---
+++ assets/js/buddyx-customizer.js
@@ -0,0 +1,10 @@
+$(document).ready(function($) {
+    $(document).on("click", '.button.buddyx-flush-font-files', function(e) {
+        e.preventDefault();
+        var data = {
+            'action': 'buddyx_regenerate_fonts_folder',
+        };
+
+        $.post(ajaxurl, data, function(response) {});
+    });
+});
