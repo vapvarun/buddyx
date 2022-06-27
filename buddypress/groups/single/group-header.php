ea43e4c9ecf203ff7ab270330d0cde08cd6caafb
Nitin Patil <pallavi@wbcomdesigns.com>
2022-06-27 11:27:14 +0530

    (#433) Managed single member cover image layout

--- buddypress/groups/single/group-header.php
+++ buddypress/groups/single/group-header.php
@@ -12,48 +12,50 @@ $group_avatar = trailingslashit( $admin_link . 'group-avatar' );
 ?>
 
 <div class="item-header-cover-image-wrapper hide-header-cover-image">
-    <div id="item-header-cover-image">
-        <?php if ( ! bp_disable_group_avatar_uploads() ) : ?>
-            <div id="item-header-avatar">
-                    <?php if ( bp_is_item_admin() ) { ?>
-                            <a href="<?php echo $group_avatar; ?>" class="link-change-profile-image bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Change Group Photo', 'buddyx' ); ?>">
-                                    <?php 
-                                        if ( function_exists('buddypress') && isset(buddypress()->buddyboss )) { ?>
-                                            <i class="bb-icon-edit-thin"></i>
-                                        <?php } else { ?>
-                                            <i class="fa fa-edit"></i>
-                                        <?php }
-                                    ?>
-                            </a>
-                    <?php } ?>
-                    <?php bp_group_avatar(); ?>
-            </div><!-- #item-header-avatar -->
-        <?php endif; ?>
+	<div id="item-header-cover-image">
+		<?php if ( ! bp_disable_group_avatar_uploads() ) : ?>
+			<div id="item-header-avatar">
+					<?php if ( bp_is_item_admin() ) { ?>
+							<a href="<?php echo $group_avatar; ?>" class="link-change-profile-image bp-tooltip" data-bp-tooltip-pos="up" data-bp-tooltip="<?php esc_attr_e( 'Change Group Photo', 'buddyx' ); ?>">
+									<?php
+									if ( function_exists( 'buddypress' ) && isset( buddypress()->buddyboss ) ) {
+										?>
+											<i class="bb-icon-edit-thin"></i>
+										<?php } else { ?>
+											<i class="fa fa-edit"></i>
+										<?php
+										}
+										?>
+							</a>
+					<?php } ?>
+					<?php bp_group_avatar(); ?>
+			</div><!-- #item-header-avatar -->
+		<?php endif; ?>
 
-    <?php if ( ! bp_nouveau_groups_front_page_description() ) : ?>
-        <div id="item-header-content">
+	<?php if ( ! bp_nouveau_groups_front_page_description() ) : ?>
+		<div id="item-header-content">
 
-            <h2 class="bp-group-title"><?php echo esc_html( bp_get_group_name() ); ?></h2> 
-            <?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>   
-                <p class="highlight group-status"><strong><?php echo esc_html( bp_nouveau_the_group_meta( array( 'keys' => 'status' ) ) ); ?></strong></p>
+			<h2 class="bp-group-title"><?php echo esc_html( bp_get_group_name() ); ?></h2> 
+			<?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>   
+				<p class="highlight group-status"><strong><?php echo esc_html( bp_nouveau_the_group_meta( array( 'keys' => 'status' ) ) ); ?></strong></p>
 			<?php } else { ?>
-                <p class="highlight group-status"><strong><?php echo wp_kses( bp_nouveau_group_meta()->status, array( 'span' => array( 'class' => array() ) ) ); ?></strong></p>
-            <?php } ?>
-            
-            <p class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_group_last_active( 0, array( 'relative' => false ) ) ); ?>">
-                <?php
-                /* translators: %s = last activity timestamp (e.g. "active 1 hour ago") */
-                printf( __( 'active %s', 'buddyx' ), bp_get_group_last_active() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                ?>
-            </p>
+				<p class="highlight group-status"><strong><?php echo wp_kses( bp_nouveau_group_meta()->status, array( 'span' => array( 'class' => array() ) ) ); ?></strong></p>
+			<?php } ?>
 
-            <?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>
-                <?php echo isset( bp_nouveau_the_group_meta()->group_type_list ) ? bp_nouveau_the_group_meta()->group_type_list : ''; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-            <?php }else { ?>
-                <?php echo isset( bp_nouveau_group_meta()->group_type_list ) ? bp_nouveau_group_meta()->group_type_list : ''; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-            <?php } ?>
+			<p class="activity" data-livestamp="<?php bp_core_iso8601_date( bp_get_group_last_active( 0, array( 'relative' => false ) ) ); ?>">
+				<?php
+				/* translators: %s = last activity timestamp (e.g. "active 1 hour ago") */
+				printf( __( 'active %s', 'buddyx' ), bp_get_group_last_active() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				?>
+			</p>
 
-            <?php
+			<?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>
+				<?php echo isset( bp_nouveau_the_group_meta()->group_type_list ) ? bp_nouveau_the_group_meta()->group_type_list : ''; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<?php } else { ?>
+				<?php echo isset( bp_nouveau_group_meta()->group_type_list ) ? bp_nouveau_group_meta()->group_type_list : ''; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			<?php } ?>
+
+			<?php
 			if ( function_exists( 'bp_group_type_list' ) ) {
 				bp_group_type_list(
 					bp_get_group_id(),
@@ -68,26 +70,26 @@ $group_avatar = trailingslashit( $admin_link . 'group-avatar' );
 			}
 			?>
 
-            <?php bp_nouveau_group_hook( 'before', 'header_meta' ); ?>
+			<?php bp_nouveau_group_hook( 'before', 'header_meta' ); ?>
 
-            <?php if ( bp_nouveau_group_has_meta_extra() ) : ?>
-                <div class="item-meta">
-                    <?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>   
-                        <?php echo bp_nouveau_the_group_meta( array( 'keys' => 'extra' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-                    <?php } else { ?>
-                        <?php echo bp_nouveau_group_meta()->extra; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
-                    <?php } ?>
-                </div><!-- .item-meta -->
-            <?php endif; ?>
+			<?php if ( bp_nouveau_group_has_meta_extra() ) : ?>
+				<div class="item-meta">
+					<?php if ( function_exists( 'bp_nouveau_the_group_meta' ) ) { ?>   
+						<?php echo bp_nouveau_the_group_meta( array( 'keys' => 'extra' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					<?php } else { ?>
+						<?php echo bp_nouveau_group_meta()->extra; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					<?php } ?>
+				</div><!-- .item-meta -->
+			<?php endif; ?>
 
-            <?php bp_nouveau_group_header_buttons(); ?>
+			<?php bp_nouveau_group_header_buttons(); ?>
 
-        </div><!-- #item-header-content -->
-    <?php endif; ?>
+		</div><!-- #item-header-content -->
+	<?php endif; ?>
 
-        <?php bp_get_template_part( 'groups/single/parts/header-item-actions' ); ?>
+		<?php bp_get_template_part( 'groups/single/parts/header-item-actions' ); ?>
 
-    </div><!-- #item-header-cover-image -->
+	</div><!-- #item-header-cover-image -->
 </div><!-- .item-header-cover-image-wrapper -->
 
 <?php if ( ! bp_nouveau_groups_front_page_description() && bp_nouveau_group_has_meta( 'description' ) ) : ?>
