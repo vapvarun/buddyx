f423417225c5fb847f181b23019979b808b59850
Nitin Patil <pallavi@wbcomdesigns.com>
2024-05-03 21:45:41 +0530

    (#631) Show featured image in search results

--- template-parts/content/entry_media.php
+++ template-parts/content/entry_media.php
@@ -5,7 +5,7 @@
  * @package buddyx
  */
 
- namespace BuddyX\Buddyx;
+namespace BuddyX\Buddyx;
 
 $allowed_post_formats = array( 'image', 'video', 'gallery', 'quote', 'link', 'audio' );
 
@@ -13,10 +13,7 @@ $post_format = get_post_format() ?: 'standard';
 $item_format = $post_format === 'standard' ? 'image' : $post_format;
 
 if ( in_array( $post_format, $allowed_post_formats, true ) ) {
-
 	get_template_part( 'template-parts/post-format/entry', $item_format );
 } else {
-	if ( ! is_search() ) {
-			get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
-	}
+	get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
 }
