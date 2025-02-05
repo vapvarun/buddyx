74b7ece45646e75e56c3a5f131be82f9f97a5648
Nitin Patil <pallavi@wbcomdesigns.com>
2025-02-05 11:46:53 +0530

    Accessibility fixes
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/8293653286

--- header.php
+++ header.php
@@ -17,7 +17,7 @@ namespace BuddyX\Buddyx;
 <head>
 	<?php do_action( 'buddyx_head_top' ); ?>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
