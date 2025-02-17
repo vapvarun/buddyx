ccf85f32fc27a8a35b56fd09d4edea77835e393d
Nitin Patil <pallavi@wbcomdesigns.com>
2025-02-17 19:15:48 +0530

    Update footer pattern
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/8237968836

--- inc/Base_Support/patterns/footer-central.php
+++ inc/Base_Support/patterns/footer-central.php
@@ -1,15 +1,5 @@
 <?php
 /**
- * Title: Central Footer with banner
- * Slug: greenshift/footer-central
- * Categories: greenshift-footer
- * Block Types: core/template-part/footer
- */
-?>
-
-
-<?php
-/**
  * Footer Central
  *
  * @package buddyx
@@ -18,7 +8,7 @@
 return array(
 	'title'      => __( 'Central Footer with banner', 'buddyx' ),
 	'categories' => array( 'buddyx-footer' ),
-    'blockTypes' => array( 'core/template-part/footer' ),
+	'blockTypes' => array( 'core/template-part/footer' ),
 	'content'    => '<!-- wp:group {"metadata":{"categories":["buddyx-footer"],"patternName":"buddyx/footer-central","name":"Central Footer with banner"},"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"100px","bottom":"40px","left":"15px","right":"15px"},"margin":{"top":"0","bottom":"0"}},"border":{"top":{"color":"#cfcfcf45","width":"1px"}},"color":{"text":"#f0f0f0"}},"backgroundColor":"black","layout":{"inherit":true,"type":"constrained"}} -->
     <div class="wp-block-group alignfull has-black-background-color has-text-color has-background has-link-color" style="border-top-color:#cfcfcf45;border-top-width:1px;color:#f0f0f0;margin-top:0;margin-bottom:0;padding-top:100px;padding-right:15px;padding-bottom:40px;padding-left:15px"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"bottom":"20px"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"x-large"} -->
     <h2 class="wp-block-heading has-text-align-center has-base-color has-text-color has-link-color has-x-large-font-size" id="let-s-do-this" style="margin-bottom:20px;font-style:normal;font-weight:600">Let\'s Do This</h2>
