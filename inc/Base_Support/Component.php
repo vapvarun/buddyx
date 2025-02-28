d0d07dda29bd70e9eee7998fda250702d9eae010
Nitin Patil <pallavi@wbcomdesigns.com>
2025-02-28 18:12:23 +0530

    Add query list pattern
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/8237968836

--- inc/Base_Support/Component.php
+++ inc/Base_Support/Component.php
@@ -299,6 +299,7 @@ class Component implements Component_Interface, Templating_Component_Interface {
 				'query-cover-featured',
 				'query-cover-grid',
 				'query-grid-excerpt',
+				'query-listbig',
 			);
 
 			foreach ( $block_patterns as $block_pattern ) {
