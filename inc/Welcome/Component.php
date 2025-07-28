0404556a8b929cafde04eb9bfd85af7acc1ed3cf
Nitin Patil <pallavi@wbcomdesigns.com>
2025-07-28 14:01:40 +0530

    Fix console error
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/8896198579

--- inc/Welcome/Component.php
+++ inc/Welcome/Component.php
@@ -244,7 +244,7 @@ class Component implements Component_Interface {
 								</div>
 								<div class="list-section-wrap-widgets">
 									<h3><?php esc_html_e( 'Community', 'buddyx' ); ?>
-										<svg width="26" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.001 2C6.47813 undefined0098 6.47715 2.00098 1undefined0098 16.9913 5.65783 21.1283 10.4385 21.8785V14.8906H7.89941V12H10.4385V9.79688C10.4385 7.29063 11.9314 5.90625 14.2156 5.90625C15.3097 5.90625 16.4541 6.10156 16.4541 6.10156V8.5625H15.1931C13.9509 8.5625 13.5635 9.33334 13.5635 10.1242V12H16.3369L15.8936 14.8906H13.5635V21.8785C18.3441 21.1283 22.001 16.9913 22.001 12C22.001 6.47715 17.5238 2 12.001 2Z"></path></svg>
+										<svg width="26" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M14 13.5H16.5L17.5 9.5H14V7.5C14 6.47062 14 5.5 16 5.5H17.5V2.1401C17.1743 2.09685 15.943 2 14.6429 2C11.9284 2 10 3.65686 10 6.69971V9.5H7V13.5H10V22H14V13.5Z"></path></svg>
 									</h3>
 									<div class="buddyx-quick-setting-section">
 									<p><?php esc_html_e( 'Join our community! Share your site, ask a question, and help others.', 'buddyx' ); ?></p>
