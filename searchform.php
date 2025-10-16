742d833bec24c8a9f55059c8334e9f58f85391bf
Nitin Patil <pallavi@wbcomdesigns.com>
2025-10-16 19:08:48 +0530

    Manage XSS in search form input value
    https://3.basecamp.com/5798509/buckets/37499979/card_tables/cards/9182597060

--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@
 ?>
 
 <form id="searchform" role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">	
-	<span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'buddyx' ); ?></span>
-	<input type="search" class="search-field-top" placeholder="<?php echo esc_attr( apply_filters( 'search_placeholder', __( 'Enter Keyword', 'buddyx' ) ) ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<input name="submit" type="submit"  value="<?php esc_attr_e( 'Go', 'buddyx' ); ?>" />
+    <span class="screen-reader-text"><?php esc_html_e( 'Search for:', 'buddyx' ); ?></span>
+    <input type="search" class="search-field-top" placeholder="<?php echo esc_attr( apply_filters( 'search_placeholder', __( 'Enter Keyword', 'buddyx' ) ) ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" />
+    <input name="submit" type="submit" value="<?php esc_attr_e( 'Go', 'buddyx' ); ?>" />
 </form>
